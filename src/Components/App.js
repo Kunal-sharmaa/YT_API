6be339e8c9ebf6c212c11d3b0d82ac64c203eec0
kunal-sharmaa <kunalsharma7678@gmail.com>
2021-02-09 20:00:50 +0530

    second commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchBar from './Searchbar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+      videos: [],
+      selectedVideo: null
+  }
+  handleSubmit = async (termFromSearchBar) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: termFromSearchBar
+      }
+    })
+  };
+  handleVideoSelect = (video) => {
+    this.setState({selectedVideo: video})
+  }
+
+  render() {
+    return (
+        <div className="ui container" style={{margintop:'lem'}}>
+          <SearchBar handleFormSubmit={this.handleSubmit}/>
+          <div className = 'ui grid'>
+            <div className = 'ui row'>
+              <div className = "eleven wide column">
+                <VideoDetail video={this.state.selectedVideo}/>
+              </div>
+              <div className = "five wide column">
+                <VideoList handleVideoSelect ={this.handleVideoSelect} videos={this.state.videos}/>
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+  }
 }
-
 export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import videoitem from './Components/VideoItem';
+import videolist from './Components/VideoLisr';
+import searchbar from './Components/SearchBar';
+import App from './Components/App';
